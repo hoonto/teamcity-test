22579a0f6c6e8e29b4c54cc56df9e034d6d4a85d
Matt Mullens <mmullens@hoonto.com>
2014-06-20 13:28:48 -0700

    adding a build task

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,24 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-  grunt.initConfig({
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/*-spec.js']
+            }
         },
-        src: ['test/*-spec.js']
-      }
-    }
-  });
-  grunt.registerTask('test', 'mochaTest');
+        jshint: {
+            files: ['./public/**/*.js'],
+            options: {
+                force: true,
+                jshintrc: './.jshintrc'
+            }
+        }
+    });
+    grunt.registerTask('test', ['jshint','mochaTest']);
+    grunt.registerTask('build', ['jshint']);
 };
